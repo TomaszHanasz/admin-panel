fb3dd65d1aaf334921998ef2c0855c21701b4f5c
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-09-04 10:01:47 -0500

    sidebar update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,56 +1,44 @@
 import React, { useState } from "react";
 import { Sidebar } from "primereact/sidebar";
 import { Button } from "primereact/button";
-import { PanelMenu } from "primereact/panelmenu";
 import { Link } from "react-router-dom";
+import "./sidebar.style.css";
 
 export default function LeftSidebar(props) {
   const [visible, setVisible] = useState(false);
 
-  const items = [
-    {
-      label: (
-        <Link to="/addDish">
-          <span>
-            <i className="pi pi-fw pi-plus"></i> Add
-          </span>
-        </Link>
-      ),
-    },
-    {
-      label: (
-        <Link to="/remove-dish">
-          <span>
-            <i className="pi pi-fw pi-trash"></i> Remove
-          </span>
-        </Link>
-      ),
-    },
-    {
-      label: (
-        <Link to="/edit-dish">
-          <span>
-            <i className="pi pi-fw pi-pencil"></i> Edit
-          </span>
-        </Link>
-      ),
-    },
-    {
-      label: (
-        <Link to="/dishList">
-          <span>
-            <i className="pi pi-fw pi-list"></i> Dish List
-          </span>
-        </Link>
-      ),
-    },
-  ];
-
   return (
     <div className="card flex justify-content-center">
       <Sidebar visible={visible} onHide={() => setVisible(false)}>
-        <div className="card flex justify-content-center">
-          <PanelMenu model={items} className="w-full md:w-25rem" />
+        <div className="sidebar__btn-group">
+          <Link to="/addDish">
+            <Button
+              label="Add Dish"
+              text
+              raised
+              severity="secondary"
+              icon="pi pi-plus"
+              style={{ width: "100%" }}
+            />
+          </Link>
+          <Link to="/dishList">
+            <Button
+              label="Dish list"
+              text
+              raised
+              severity="secondary"
+              icon="pi pi-list"
+              style={{ width: "100%" }}
+            />
+          </Link>
+          <Button
+            label="Log Out"
+            text
+            raised
+            severity="secondary"
+            icon="pi pi-sign-out"
+            style={{ width: "100%" }}
+          />
         </div>
       </Sidebar>
       <Button icon="pi pi-arrow-right" onClick={() => setVisible(true)} />
