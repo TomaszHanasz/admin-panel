8a1f9f00a96e0ce90f64ed343c793c0bfb45d494
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-09-23 12:36:17 -0500

    ingredients updated

--- src/data/ingredients.js
+++ src/data/ingredients.js
@@ -1,9 +1,43 @@
 const ingredientsList = [
+  //meats
+  { name: "Chicken" },
+  {name: "Beef"},
+  {name:"Fish"},
+  {name:"Lamb"},
+  {name:"Turkey"},
+  //veggies
   { name: "Eggs" },
   { name: "Carrot" },
   { name: "Onion" },
   { name: "Broccoli" },
-  { name: "Chicken" },
+  { name: "Sweet Pepper" },
+  {name: "Spicy Pepper"},
+  { name: "Tomato" },
+  { name: "Cucumber" },
+  { name: "Cilantro" },
+  { name: "Coriander" },
+  { name: "Potato" },
+  { name: "Beets" },
+  { name: "Dill" },
+  { name: "Garlic" },
+  { name: "Celery" },
+  { name: "Parsley" },
+  { name: "Green Lettuce" },
+  {name: "Cabbage"},
+
+
+  //breakfast
+
+  {name:"Honey"},
+  {name:"Milk"},
+  {name:"Sugar"},
+  {name:"Sweet Cream"},
+  {name:"Whipped Cream"},
+
+
+  
+  
+  
 ];
 
 export { ingredientsList };
