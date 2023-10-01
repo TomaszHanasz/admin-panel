4dce5e7dce8df5507b61b2d4d2da557277d3e3c7
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-10-01 11:30:56 -0500

    logo update

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Card } from "primereact/card";
+import logo from "./109_509195019.png";
 import "./panel.style.css";
 
 export default function Panel() {
   return (
     <div>
-      <Card className="panel__card" style={{ textAlign: "center" }}>
-        Welcome
+      <Card
+        className="panel__card"
+        style={{ textAlign: "center", boxShadow: "none" }}
+      >
+        <img src={logo} alt="cafe alif logo" />
       </Card>
     </div>
   );
